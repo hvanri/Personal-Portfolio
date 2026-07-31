b3e696d624aa15a3d07b412ea02416df5f90412f
Ri Ha Van <rihv2@magenest.com>
2026-07-31 11:36:44 +0700

    POR-15: Update url more friendlier

--- js/core/router.js
+++ js/core/router.js
@@ -5,7 +5,32 @@ export function initTransitions() {
         link.addEventListener('click', e => {
             e.preventDefault();
             document.body.classList.add('page-transitioning');
-            setTimeout(() => location.href = link.href, 300);
+            setTimeout(() => location.href = getNavigationHref(link), 300);
         });
     });
+}
+
+function getNavigationHref(link) {
+    const url = new URL(link.href);
+    if (!isLocalStaticServer()) {
+        return url.href;
+    }
+
+    const localRoutes = {
+        '/': '/index.html',
+        '/about': '/about.html',
+        '/blog': '/blog.html',
+        '/projects': '/projects.html'
+    };
+    const path = url.pathname.replace(/\/$/, '') || '/';
+
+    if (localRoutes[path]) {
+        url.pathname = localRoutes[path];
+    }
+
+    return url.href;
+}
+
+function isLocalStaticServer() {
+    return ['127.0.0.1', 'localhost'].includes(window.location.hostname);
 }
